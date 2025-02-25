53cd4941d468a3a60c2746e57d3416d2f6b89324
muhidtech <muhideen894@gmail.com>
2025-02-25 09:22:46 +0000

    25th Feb 2025

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,13 +7,13 @@ function Footer() {
     const d = new Date();
   
     return (
-        <div className='w-full flex items-center lg:px-30 px-20 py-10 justify-between'>
+        <div className='w-full flex items-center lg:px-30 px-20 py-10 justify-between max-md:px-5 max-md:gap-x-10'>
             <div className='text-left flex-grow'>
                 <p className='text-gray-600 text-md'>
                     Copyright &copy; {d.getFullYear()} TaxPal. All rights reserved.
                 </p>
             </div>
-            <div className='space-x-4 flex'>
+            <div className='space-x-4 max-md:space-x-0 flex'>
                 <a href='#' className='text-gray-400 hover:text-gray-600'>
                     <XMarkIcon className='w-6 h-6' />
                 </a>
